eb810e3d28f42fa1faba7d4874ae4e5677442be7
SwiftBeta <swiftbeta.blog@gmail.com>
2023-11-17 16:31:28 +0100

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,32 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Home on 17/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NotasApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
